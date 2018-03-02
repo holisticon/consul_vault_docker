b22ced3cdaab5bd158e5d98ddd0205820f9350be
Martin Reinhardt <contact@martinreinhardt-online.de>
2018-03-02 19:33:11 +0100

    fix(build): Try to make travis happy again

--- docker-build.sh
+++ docker-build.sh
@@ -14,7 +14,7 @@ cp -rp docker/src/consul/data/ docker/build/container/consul/data/
 ## Start new containers
 docker-compose up -d --build
 
-sleep 10
+sleep 60
 # unseal vault
 docker exec -it vault_server vault unseal aMf3xmMkRzGJYPSCF2A534yBx4QwEGUjNbWRLoTL+pav
 docker exec -it vault_server vault unseal TvK0mwG6ZSKVJEjvLMlaxjXnbZGb+QLXPu837zZO9HjN
